cade26c9429bd2a763952bd2db7b5bc0fe999c9b
Yusuf Khasbulatov <khashashin@gmail.com>
2024-05-21 01:16:52 +0200

    Update base URL configuration for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,9 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
+      includes: "_includes",
       output: "_site"
-    }
+    },
+    pathPrefix: "/"
   };
 };
